1e174a1f05f3c181efb3a34a5e14da66605823c2
RedZoo <hyunsim01@naver.com>
2024-10-02 12:40:14 +0900

    오페라 하우스 맵 추가 2

---
+++ Source/EraOfDreams/EraOfDreamsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EraOfDreamsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEraOfDreamsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEraOfDreamsGameMode();
+};
+
+
+
